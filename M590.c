cc8155e9db6910b2612b5fc6f5ab59f8792efb8a
Milan <milanputinci08@gmail.com>
2018-11-18 14:07:08 +0100

    modified:   M590.c 	modified:   M590.h 	modified:   MotoriKapije.h 	new file:   delay.h 	modified:   main.c

--- M590.c
+++ M590.c
@@ -1,74 +1,164 @@
 #include "M590.h"
 
+
+
 bool SIM_ON()
+
 {
+
 	bool state_sim=false;
+
 	_delay_ms(1000);
+
 	
+
 	simOn;
+
 	_delay_ms(1500);
+
 	uart_clear();
+
 	while(!state_sim)
+
 	{
+
 		if(uart_full())
+
 		{
+
 			if(strcmp("\r\n+PBREADY\r\n",Resive)==0)
+
 			state_sim=true;
+
 		}
+
 	}
+
 	return true;
+
 }
+
+
 
 void SIM_OFF()
+
 {
+
 	simOff;
+
 }
+
+
 
 void SIM_SendSms(char *Message,char *Number)
+
 {
+
 	
+
 	char num[50]="AT+CMGS=\"";
+
 	strcat(num,Number);
+
 	strcat(num,"\"\r");
+
 	
+
 	
+
 	uart_send_string("AT+CSCS=\"GSM\"\r");
+
 	_delay_ms(2000);
+
 	uart_send_string("AT+CMGF=1\r");
+
 	_delay_ms(2000);
+
 	
+
 	uart_send_string(num);
+
 	_delay_ms(1000);
+
 	uart_send_string(Message);
+
 	_delay_ms(20);
+
 	UDR=sms_end;
+
 	_delay_ms(1000);
+
 }
+
+
 
 void SIM_delate_message()
+
 {
+
 	_delay_ms(500);
+
 	uart_send_string("AT+CMGD=0\r");
+
 	_delay_ms(500);
+
 	uart_send_string("AT+CMGD=1\r");
+
 	_delay_ms(500);
+
 	uart_send_string("AT+CMGD=2\r");
+
 	_delay_ms(500);
+
 	uart_send_string("AT+CMGD=3\r");
+
 	_delay_ms(500);
+
 	uart_send_string("AT+CMGD=4\r");
+
 	_delay_ms(500);
+
 }
 
+
+
 void SIM_Inic()
+
 {
+
 	Uart_inic();
+
 	sei();
+
 	
+
 	simOff;
+
 	_delay_ms(500);
+
 	DDRD|=(1<<4);
+
 	_delay_ms(500);
+
 	SIM_ON();
+
 	
+
+}
+
+bool SIM_Call_check()
+{
+	_delay_ms(500);
+	if(strcmp(Resive,"\r\nRING\r\n")==0)
+	return true;
+	return false;
+	
+}
+
+void SIM_Hold_call()
+{
+	_delay_ms(200);
+	uart_send_string("AT+CHLD=0\r");
+	_delay_ms(500);
+	uart_clear();
 }
--- MotoriKapije.h
+++ MotoriKapije.h
@@ -1,94 +1,190 @@
 /*
+
  * MotoriKapije.h
+
  *
+
  * Created: 27/09/2018 11:05:23
+
  *  Author: Bozic
+
  */ 
-#include <util/delay.h>
+
+#include "delay.h"
+#include <stdint.h>
+
+
 
 #define M1_ON1 PORTD |=(1<<7)
+
 #define M1_ON2 PORTC |=(1<<0)
+
 #define M1_OFF1 PORTD &=~(1<<7)
+
 #define M1_OFF2 PORTC &=~(1<<0)
 
+
+
 #define M2_ON1 PORTD |=(1<<5)
+
 #define M2_ON2 PORTD |=(1<<6)
+
 #define M2_OFF1 PORTD &=~(1<<5)
+
 #define M2_OFF2 PORTD &=~(1<<6)
 
+
+
 #ifndef MOTORIKAPIJE_H_
+
 #define MOTORIKAPIJE_H_
+
+
 
 //extern bool TrenutnoStanje;
 
+
+
 enum Kapija {Otvoreno,Zatvoreno};
+
 	
+
 extern enum Kapija Kapija1;
 
 
+
+
+
 void OtvoriLevo()
+
 {
+
 	M1_ON1;
+
 	M1_OFF2;
+
 }
+
 void OtvoriDesno()
+
 {
+
 	M2_ON1;
+
 	M2_OFF2;
+
 }
+
 void ZatvoriLevo()
+
 {
+
 	M1_OFF1;
+
 	M1_ON2;
+
 }
+
 void ZatvoriDesno()
+
 {
+
 	M2_OFF1;
+
 	M2_ON2;
+
 }
+
+
 
 void OtvoriKapiju()
+
 {
+
 	if(Kapija1==Zatvoreno)
+
 	{
+
 		OtvoriLevo();
+		_delay_ms(100);
+
 		OtvoriDesno();
+
 		Kapija1=Otvoreno;
+
 		_delay_ms(500);
+
 	}	
+
 }
+
 void ZatvoriKapiju()
+
 {
+
 	if(Kapija1==Otvoreno)
+
 	{
+
 		ZatvoriLevo();
+		_delay_ms(100);
+
 		ZatvoriDesno();
+
 		Kapija1=Zatvoreno;
+
 		_delay_ms(500);
+
 	}
+
 }
+
+
 
 int ADCsingleREAD(uint8_t adctouse) //adctouse koji pin treba da se obradi
+
 {
+
 	int ADCval;
 
+
+
 	ADMUX = adctouse;         // use #5 ADC
+
 	ADMUX |= (1 << REFS0);    // use AVcc as the reference
+
 	ADMUX &= ~(1 << ADLAR);   // clear for 10 bit resolution
+
 	
+
 	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);    // 128 prescale brzina konverzije
+
 	ADCSRA |= (1 << ADEN);    // Enable the ADC
 
+
+
 	ADCSRA |= (1 << ADSC);    // Start the ADC conversion
+
+
 
 	while(ADCSRA & (1 << ADSC));      // Thanks T, this line waits for the ADC to finish
 
 
+
+
+
 	ADCval = ADCL;
+
 	ADCval = (ADCH << 8) + ADCval;    // ADCH is read so ADC can be updated again
 
+
+
 	return ADCval;
+
 }
+
+
+
 
 
 #endif /* MOTORIKAPIJE_H_ */
